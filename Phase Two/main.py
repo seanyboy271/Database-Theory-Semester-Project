b4beceac925b2bf316ed0002cd9897018c2c805b
seanyboy271 <sean.kotrola@gmail.com>
2019-11-22 17:43:52 -0500

    Added connection string for cloud proxy

--- Phase Two/main.py
+++ Phase Two/main.py
@@ -12,34 +12,41 @@ cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
 
 app = Flask(__name__)
 
-db = sqlalchemy.create_engine(
-    # Equivalent URL:
-    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
-    sqlalchemy.engine.url.URL(
-        drivername='mysql+pymysql',
-        username=db_user,
-        password=db_pass,
-        database=db_name,
-        query={
-            'unix_socket': '/cloudsql/{}'.format(cloud_sql_connection_name)
-        }
-    )
-)
-# [END cloud_sql_mysql_sqlalchemy_create]
+
+##this is for testing
+db = sqlalchemy.create_engine("mysql+pymysql://root:password@/test?127.0.0.1/" + str(cloud_sql_connection_name))
+
+##This is for deployment
+# db = sqlalchemy.create_engine(
+#     # Equivalent URL:
+#     # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+#     sqlalchemy.engine.url.URL(
+#         drivername='mysql+pymysql',
+#         username=db_user,
+#         password=db_pass,
+#         database=db_name,
+#         query={
+#             'unix_socket': '/cloudsql/{}'.format(cloud_sql_connection_name)
+#         }
+#     )
+# )
 
 
+## We should have a route for each query
 @app.route('/', methods=["GET"])
 def main():
 
-    try: 
-        with db.connect() as conn:
-            num = conn.execute("Select * from testTable").fetchall()
-            return str(num)
+    return render_template("index.html")
 
-    except Exception as e:
-        return "it broke" + str(e)
+    # try: 
+    #     with db.connect() as conn:
+    #         num = conn.execute("Select * from testTable").fetchall()
+    #         return str(num)
+
+    # except Exception as e:
+    #     return "it broke " + str(e)
 
         
 
 if __name__=='__main__':    
-    app.run()
+    app.run(debug=True)
