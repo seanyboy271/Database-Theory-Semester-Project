ab6467289fe10833dbd1e54575205ef4d2335841
Ian Burns <burnsie@vcu.edu>
2019-11-28 17:30:56 -0600

    Get Cable Color For Specific Car

--- Phase Two/static/js/vehicle.js
+++ Phase Two/static/js/vehicle.js
@@ -1,11 +1,23 @@
 function VehicleDetailViewModel(res) {
     self = this;
 
-    self.vehicle = ko.observable(JSON.parse(res));
+    self.vehicle = ko.observable(JSON.parse(res)[0]);
+    self.cableColor = ko.observable("Loading...");
 
-    console.log(res);
-    console.log(JSON.parse(res));
-    console.log(self.vehicle);
+    self.getCableColor = function() {
+        $.ajax({
+            dataType: "json",
+            url: '/CradlepointCableColor?unitNumber=' + self.vehicle().unitNumber,
+            success: function (data, textStatus, XmlHttpRequest) {
+                self.cableColor(data[0].cableColor);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    };
+
+    self.getCableColor();
 
     return self;
 }
